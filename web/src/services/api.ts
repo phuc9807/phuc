52ce830a03c08bb20608794d83b24a4e6c2d9616
phuc9807 <phuc2babon@gmail.com>
2026-05-19 13:18:45 +0700

    Update api.ts

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tusigma-api-production.up.railway.app/api',
+  baseURL: 'https://tusigma-api-production.up.railway.app',
 });
 
 api.interceptors.request.use((config) => {
